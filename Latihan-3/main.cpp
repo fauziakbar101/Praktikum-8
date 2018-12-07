2db9c3f238314d5e8f852a3d5ee4c0799ec695f6
Fauziakbar101 <fakabr7@gmail.com>
2018-12-07 22:34:54 +0700

    rekursif dan iteratif

---
+++ Latihan-3/main.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+ #include <iostream>
+
+using namespace std;
+ int kali1(int a, int b){
+ int hasil =0;
+ for(int i=0;i<b;i++){
+ hasil=hasil+a;
+ }
+ return hasil;
+ }
+ int kali2(int a, int b){
+ if(b==0)
+ return 1;
+ else if(b==1)
+ return a;
+ else
+ return a+kali2(a,b-1);
+ }
+ int main(int argc, char *argv[])
+ {
+ int a,b;
+ cout<<"masukkan a :";
+ cin>>a;
+ cout<<"masukkan b :";
+ cin>>b;
+ cout<<"secara ITERATIF :"<<endl;
+ cout<<kali1(a,b)<<endl;
+ cout<<"secara REKURSIF"<<endl;
+ cout<<kali2(a,b)<<endl;
+ system("PAUSE");
+cout <<"=================================================="<<endl;
+    cout << "Nama  : Fauzi Akbar"<<endl;
+cout <<"=================================================="<<endl;
+    cout << "NIM   : 311810855"<<endl;
+cout <<"=================================================="<<endl;
+    cout << "Kelas : TI.18.C.2"<<endl;
+cout <<"=================================================="<<endl;
+  return EXIT_SUCCESS;;
+}
